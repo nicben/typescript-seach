ab83b7f79581831963b66635ef7afa488af335f1
Nicole Bendu <nic.bendu@gmail.com>
2021-09-06 18:46:26 +0200

    CHORE: created ship interface

---
+++ src/IShip.ts
@@ -0,0 +1,6 @@
+export interface IShip {
+  shipId: string;
+  heading: string;
+  body: string;
+  yearOfConstruction: string;
+}
---
+++ src/api.ts
@@ -0,0 +1,1 @@
+export const api: string = 'http://localhost:4000/api/ships/';
